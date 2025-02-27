a5b93fe962c6a701792f162e46c7d11ef2139be0
meganmlee <minjul0501@gmail.com>
2025-02-27 17:55:04 -0500

    Remove build artifacts from repository

--- ros2_ws/build/kortex_driver/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/megan/magic/ros2_ws/install/control_msgs/share/control_msgs/package.sh"
-. "/home/megan/magic/ros2_ws/install/kortex_api/share/kortex_api/package.sh"
-. "/home/megan/magic/ros2_ws/install/ros2_control_test_assets/share/ros2_control_test_assets/package.sh"
-. "/home/megan/magic/ros2_ws/install/hardware_interface/share/hardware_interface/package.sh"
