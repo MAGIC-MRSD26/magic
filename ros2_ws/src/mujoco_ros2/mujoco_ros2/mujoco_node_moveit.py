f722bd95e8fcd59aadba6b4e2929cf336923a2ff
kailashjagadeesh <kailashjagadeesh2000@gmail.com>
2025-03-15 00:09:07 -0400

    fixing path issues

--- ros2_ws/src/mujoco_ros2/mujoco_ros2/mujoco_node_moveit.py
+++ ros2_ws/src/mujoco_ros2/mujoco_ros2/mujoco_node_moveit.py
@@ -7,12 +7,18 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 import numpy as np
-from one_arm_pid_control import SingleArmPIDControl
+import sys
+import os
+from ament_index_python.packages import get_package_share_directory
+
+sys.path.append('../')
+from control.one_arm_pid_control import SingleArmPIDControl
 
 class MuJoCoNode(Node):
     def __init__(self):
         super().__init__('mujoco_node')
-        self.xml_filepath = "/home/jarvis/codebase/magic_capstone/magic/manipulation/kinova_gen3_mujoco/two_arm_table.xml"
+        control_package_path=get_package_share_directory('control')
+        self.xml_filepath=os.path.join(control_package_path,'kinova_gen3_mujoco/two_arm_vention_table.xml')
         self.model = mujoco.MjModel.from_xml_path(self.xml_filepath)
         self.data = mujoco.MjData(self.model)
         self.model.opt.timestep = 0.001
