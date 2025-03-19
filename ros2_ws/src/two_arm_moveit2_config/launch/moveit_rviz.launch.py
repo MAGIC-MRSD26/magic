7e2515c97f3f151d6e3898b9e4fc6f7f41c9efe5
meganmlee <minjul0501@gmail.com>
2025-03-18 23:18:47 -0400

    get rid of unused launch files

--- ros2_ws/src/two_arm_moveit2_config/launch/moveit_rviz.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("gen3_robotiq_2f_85", package_name="two_arm_moveit2_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
