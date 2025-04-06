a6c03a254ed1ffa07309fd4269d531eb10e260cd
meganmlee <minjul0501@gmail.com>
2025-04-05 21:28:03 -0400

    finish fsm for pick and place

--- ros2_ws/src/moveit_go/src/motion_planning.cpp
+++ ros2_ws/src/moveit_go/src/motion_planning.cpp
@@ -23,6 +23,7 @@ enum class State {
     MOVE_TO_LIFT,
     PLAN_TO_PLACE,
     MOVE_TO_PLACE,
+    PLACE,
     PLAN_TO_HOME,
     MOVE_TO_HOME,
     SUCCEEDED,
@@ -62,9 +63,10 @@ public:
                 // arm_move_group_.move();
 
                 // Wait for pose if needed
-                if (!grasp_pose_received_) {
+                if (!grasp_pose_received_ && retry_count < 3) {
                     RCLCPP_INFO_THROTTLE(LOGGER, *node_->get_clock(), 2000, 
                                         "Waiting for pose on /left_arm_pose...");
+                    retry_count++;
                     return true; // Stay in HOME state until we get a message
                 }
                 current_state_ = State::PLAN_TO_OBJECT;
@@ -98,6 +100,9 @@ public:
             case State::MOVE_TO_PLACE:
                 return moveToPlace();
 
+            case State::PLACE:
+                return Place();
+
             case State::PLAN_TO_HOME:
                 return planToHome();
 
@@ -120,6 +125,7 @@ public:
     }
 
 private:
+    int retry_count = 0;
     rclcpp::Node::SharedPtr node_;
     std::string arm_planning_group_;
     std::string gripper_planning_group_;
@@ -129,6 +135,9 @@ private:
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
     State current_state_;
     moveit::planning_interface::MoveGroupInterface::Plan current_plan_;
+
+    geometry_msgs::msg::Pose target_pose;
+    moveit_msgs::msg::AttachedCollisionObject attached_bin;
 
     // Grasp pose subscription and storage
     rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr grasp_pose_subscription_;
@@ -155,7 +164,6 @@ private:
         geometry_msgs::msg::Quaternion quat_orient;
         tf2::convert(tf2_quat, quat_orient);
 
-        geometry_msgs::msg::Pose target_pose;
         target_pose.orientation = quat_orient;
 
         if (grasp_pose_received_) {
@@ -163,13 +171,12 @@ private:
             target_pose.position.x = target_grasp_pose_.position.x;
             target_pose.position.y = target_grasp_pose_.position.y;
             target_pose.position.z = target_grasp_pose_.position.z; // Slightly above the grasp position
+        } else {
+            RCLCPP_INFO(LOGGER, "Using default position");
+            target_pose.position.x = 0.1868; // Default position
+            target_pose.position.y = 0.0;
+            target_pose.position.z = 1.4; // Slightly above the grasp position
         }
-        // } else {
-        //     RCLCPP_INFO(LOGGER, "Using default position");
-        //     target_pose.position.x = 0.1868; // Default position
-        //     target_pose.position.y = 0.0;
-        //     target_pose.position.z = 1.4; // Slightly above the grasp position
-        // }
         arm_move_group_.setPoseTarget(target_pose);
         arm_move_group_.setPlanningTime(15.0);  // Give the planner more time (15 seconds)
         auto const success = static_cast<bool>(arm_move_group_.plan(current_plan_));
@@ -293,30 +300,7 @@ private:
 
     bool Grasp() {
 
-        // // Allow Collision
-        // std::vector<std::string> touch_links;
-        // touch_links.push_back("left_robotiq_85_right_finger_tip_link");
-        // touch_links.push_back("left_robotiq_85_left_finger_tip_link");
-        // touch_links.push_back("left_robotiq_85_right_inner_knuckle_link");
-        // touch_links.push_back("left_robotiq_85_left_inner_knuckle_link");
-        // touch_links.push_back("left_robotiq_85_right_knuckle_link");
-        // touch_links.push_back("left_robotiq_85_left_knuckle_link");
-        
-        // // Get the current planning scene
-        // moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-        
-        // // Simply modify the ACM (Allowed Collision Matrix) for the bin and gripper links
-        // for (const auto& link : touch_links) {
-        //     planning_scene_interface.allowCollisions("bin", link, true);
-        // }
-
-        // Close gripper
-        gripper_move_group_.setNamedTarget("Close");
-        gripper_move_group_.move();
-
         // Attach object to gripper
-        visual_tools_.prompt("Press 'Next' to attach object to gripper");
-        moveit_msgs::msg::AttachedCollisionObject attached_bin;
         attached_bin.object.id = "bin";
         attached_bin.link_name = arm_move_group_.getEndEffectorLink();
         
@@ -329,15 +313,24 @@ private:
         
         // Define object as attached
         attached_bin.object.operation = moveit_msgs::msg::CollisionObject::ADD;
-        
-        // Attach the bin
-        bool success = (planning_scene_interface_.applyAttachedCollisionObject(attached_bin) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+        bool attach_success = (planning_scene_interface_.applyAttachedCollisionObject(attached_bin) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
-        if (success) {
-            RCLCPP_INFO(LOGGER, "Graped");
+        // Close gripper
+        gripper_move_group_.setNamedTarget("Close");
+        bool gripper_success = (gripper_move_group_.move() == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+        // Check both operations for overall success
+        if (attach_success && gripper_success) {
+            RCLCPP_INFO(LOGGER, "Successfully grasped object");
+            visual_tools_.prompt("Press 'Next' to plan lifting the object");
             current_state_ = State::PLAN_TO_LIFT;
         } else {
-            RCLCPP_ERROR(LOGGER, "Failed to grasp");
+            if (!attach_success) {
+                RCLCPP_ERROR(LOGGER, "Failed to attach object");
+            }
+            if (!gripper_success) {
+                RCLCPP_ERROR(LOGGER, "Failed to close gripper");
+            }
             current_state_ = State::FAILED;
         }
         
@@ -345,36 +338,133 @@ private:
     }
 
     bool planToLift() {
-
-        RCLCPP_INFO(LOGGER, "Planning to lift");
-        current_state_ = State::MOVE_TO_LIFT;
+        // Get current pose as starting point
+        geometry_msgs::msg::PoseStamped current_pose = arm_move_group_.getCurrentPose();
+        
+        // Add to z position
+        geometry_msgs::msg::Pose lift_pose = current_pose.pose;
+        lift_pose.position.z += 0.2;  // Lift by 20cm
+        
+        arm_move_group_.setPoseTarget(lift_pose);
+        arm_move_group_.setPlanningTime(15.0);  // Give the planner more time
+        
+        auto const success = static_cast<bool>(arm_move_group_.plan(current_plan_));
+        
+        if (success) {
+            RCLCPP_INFO(LOGGER, "Successfully planned to lift");
+            visual_tools_.prompt("Press 'Next' to move to lift pose");
+            current_state_ = State::MOVE_TO_LIFT;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Failed to plan to lift");
+            current_state_ = State::FAILED;
+        }
+        
         return true;
     }
 
     bool moveToLift() {
 
-        RCLCPP_INFO(LOGGER, "Moving to lift");
-        current_state_ = State::PLAN_TO_PLACE;
+        bool success = (arm_move_group_.execute(current_plan_) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+        
+        if (success) {
+            RCLCPP_INFO(LOGGER, "Successfully moved to lift pose");
+            visual_tools_.prompt("Press 'Next' to plan to place");
+            current_state_ = State::PLAN_TO_PLACE;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Failed to move to lift pose");
+            current_state_ = State::FAILED;
+        }
+        
         return true;
     }
 
     bool planToPlace() {
-
-        RCLCPP_INFO(LOGGER, "Planning to place");
-        current_state_ = State::MOVE_TO_PLACE;
+        target_pose.position.z = 1.35;
+        arm_move_group_.setPoseTarget(target_pose);
+        arm_move_group_.setPlanningTime(15.0);
+        
+        auto const success = static_cast<bool>(arm_move_group_.plan(current_plan_));
+        
+        if (success) {
+            RCLCPP_INFO(LOGGER, "Successfully planned to place position");
+            visual_tools_.prompt("Press 'Next' to execute placement motion");
+            current_state_ = State::MOVE_TO_PLACE;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Failed to plan to place position");
+            current_state_ = State::FAILED;
+        }
+        
         return true;
     }
 
     bool moveToPlace() {
 
-        RCLCPP_INFO(LOGGER, "Moving to place");
-        current_state_ = State::PLAN_TO_HOME;
+        bool success = (arm_move_group_.execute(current_plan_) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    
+        if (success) {
+            RCLCPP_INFO(LOGGER, "Successfully moved to place position");
+            visual_tools_.prompt("Press 'Next' to drop object");
+            current_state_ = State::PLACE;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Failed to move to drop position");
+            current_state_ = State::FAILED;
+        }
+        
+        return true;
+    }
+
+    bool Place() {
+
+        // geometry_msgs::msg::PoseStamped current_pose = arm_move_group_.getCurrentPose();
+        // geometry_msgs::msg::Pose put_down_pose = current_pose.pose;
+        // put_down_pose.position.z -= 0.05;  // Move slightly down
+        
+        // // Use Cartesian path for more controlled motion
+        // std::vector<geometry_msgs::msg::Pose> waypoints;
+        // waypoints.push_back(put_down_pose);
+        
+        // moveit_msgs::msg::RobotTrajectory trajectory;
+        // const double jump_threshold = 0.0;
+        // const double eef_step = 0.01;  // Small step size for smooth motion
+        
+        // // Plan the Cartesian path
+        // double fraction = arm_move_group_.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+        
+        // if (fraction > 0.9) {  // If we can achieve at least 90% of the path
+        //     // Execute the downward motion
+        //     current_plan_.trajectory_ = trajectory;
+        //     bool put_down_success = (arm_move_group_.execute(current_plan_) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+            
+        //     if (!put_down_success) {
+        //         RCLCPP_ERROR(LOGGER, "Failed to execute put-down motion");
+        //         current_state_ = State::FAILED;
+        //         return true;
+        //     }
+        // } else {
+        //     RCLCPP_WARN(LOGGER, "Could not compute full put-down path, proceeding with release");
+        // }
+
+
+        bool success = (arm_move_group_.detachObject(attached_bin.object.id) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+        // Open gripper
+        gripper_move_group_.setNamedTarget("Open");
+        gripper_move_group_.move();
+
+        if (success) {
+            RCLCPP_INFO(LOGGER, "Successfully dropped object");
+            visual_tools_.prompt("Press 'Next' to plan to home");
+            current_state_ = State::PLAN_TO_HOME;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Failed to drop object");
+            current_state_ = State::FAILED;
+        }
+        
         return true;
     }
 
     bool planToHome() {
 
-        visual_tools_.prompt("Press 'Next' to plan to home");
         arm_move_group_.setNamedTarget("Home");
         arm_move_group_.setPlanningTime(15.0);
         auto const success = static_cast<bool>(arm_move_group_.plan(current_plan_));
