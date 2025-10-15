35d1f2cc0727ec8ee73a8d58a481deb3667b0fa1
kailashjagadeesh <kailashjagadeesh2000@gmail.com>
2025-10-15 18:13:10 -0400

    added timestamp for saving

--- ros2_ws/src/moveit_go/src/transforms.py
+++ ros2_ws/src/moveit_go/src/transforms.py
@@ -5,7 +5,8 @@ from tf2_ros import Buffer, TransformListener
 from tf_transformations import quaternion_matrix, quaternion_from_matrix
 from geometry_msgs.msg import Pose
 import numpy as np
-
+import time
+import os, csv
 def pose_to_matrix(p: Pose):
     T = quaternion_matrix([p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w])
     T[0:3, 3] = [p.position.x, p.position.y, p.position.z]
@@ -35,6 +36,8 @@ class DualArmObjectPose(Node):
         super().__init__('dual_arm_object_pose')
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.log_file = "object_pose_log.csv"
+        self._setup_csv()
 
         # known offsets at grasp time (tune or calibrate once)
         self.left_T_obj = np.eye(4)
@@ -43,6 +46,14 @@ class DualArmObjectPose(Node):
         self.right_T_obj[0,3] = -0.00
 
         self.timer = self.create_timer(0.5, self.compute_object_pose)
+
+    def _setup_csv(self):
+        """Create CSV with headers if not already present."""
+        
+        if not os.path.exists(self.log_file):
+            with open(self.log_file, mode='w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(["timestamp", "x", "y", "z", "qx", "qy", "qz", "qw"])
 
     def compute_object_pose(self):
         try:
@@ -65,6 +76,22 @@ class DualArmObjectPose(Node):
             pose_L = matrix_to_pose(T_WO_L)
             pose_R = matrix_to_pose(T_WO_R)
             pose_avg = average_pose(pose_L, pose_R)
+
+            #writing to csv:
+            ts = time.time_ns()
+            with open(self.log_file, mode='a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    f"{ts:.6f}",
+                    pose_avg.position.x,
+                    pose_avg.position.y,
+                    pose_avg.position.z,
+                    pose_avg.orientation.x,
+                    pose_avg.orientation.y,
+                    pose_avg.orientation.z,
+                    pose_avg.orientation.w
+                ])
+
             # extract rotation matrix of averaged pose
             q = [pose_avg.orientation.x, pose_avg.orientation.y,
                 pose_avg.orientation.z, pose_avg.orientation.w]
