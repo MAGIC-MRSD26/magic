1fc60b556fedbcd65e15963ae12bf860319a423f
meganmlee <minjul0501@gmail.com>
2025-11-10 17:00:46 -0500

    fix

--- ros2_ws/src/moveit_go/src/object_definitions.cpp
+++ ros2_ws/src/moveit_go/src/object_definitions.cpp
@@ -59,7 +59,7 @@ ObjectParameters ObjectFactory::createTbarParameters(double x, double y, double 
     params.rotation_angle = rotation_angle; 
     params.object_id = "tbar";
     params.approach_offset = 0.035;
-    params.grasp_offset = -0.21;
+    params.grasp_offset = -0.23;
     
     // Calculate grasp poses
     calculateGraspPoses(ObjectType::TBAR, params);
@@ -111,10 +111,10 @@ void ObjectFactory::calculateGraspPoses(ObjectType type, ObjectParameters& param
         // Calculate grasp positions for diagonal spokes
         double grasp_distance;
         if (type == ObjectType::CYLINDER_WITH_SPOKES) {
-            grasp_distance = params.spoke_length + params.cylinder_radius + 0.02;
+            grasp_distance = params.spoke_length + params.cylinder_radius + 0.01;
         } else if (type == ObjectType::TBAR) {
             double stem_radius = params.stem_width / 2;
-            grasp_distance = stem_radius + params.spoke_length + 0.125;
+            grasp_distance = stem_radius + params.spoke_length + 0.11;
         }
 
         // Grasp height 
